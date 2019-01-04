fd8be8c10734fd601ce9fccfe93ca977a009e0f3
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-01-04 11:30:51 -0600

    : CORRECTION
    OF THE SYNTAX-FAULTS WITHIN THE CERTIFICATION-CLAIMS BY THE LACK OF THE CLOSURES.

--- html/studio/script/psg.js
+++ html/studio/script/psg.js
@@ -9,19 +9,38 @@ var PSG = {
     thinkingTerm: ["IS", "ARE", "IS:", "ARE:"],
     positionCatalog: {},
     createPosition: function(a, b){
-        PSG.positionCatalog[a] = b;
-        PSG.positionCatalog[b] = a;
+        PSG.positionCatalog[a] = b || a;
         PSG.positionTerm.push(a);
-        PSG.positionTerm.push(b);
+        if (b) {
+            PSG.positionTerm.push(b);
+            PSG.positionCatalog[b] = a;    
+        }
     }
 };
 
 PSG.createPosition("FOR", "BY");
 PSG.createPosition("WITH", "OF");
-PSG.createPosition("IN", "OUT");
-PSG.createPosition("ON", "OFF");
-PSG.createPosition("THROUGH", "ROUND");
-PSG.createPosition("WITHIN", "OUTSIDE");
+
+// these words don't have backward analogs which make sense.
+// by, for of and with should be sufficient for checking the work;
+// otherwise we'd need an extensive word catalog of analogs for fact-words;
+// and that might not even work... not all words have analogs;
+// on the 4th-day is not off the 4th-day... etc
+// -- Triston-Jerard: Taylor
+
+PSG.createPosition("AS"); // as is always as in a mirror
+//PSG.createPosition("IN", "OUT"); // non-sense
+PSG.createPosition('IN');
+PSG.createPosition('OUT');
+//PSG.createPosition("ON", "OFF"); // non-sense
+PSG.createPosition('ON');
+PSG.createPosition('OFF');
+//PSG.createPosition("THROUGH", "ROUND"); // non-sense
+PSG.createPosition("THROUGH"); 
+PSG.createPosition("ROUND"); // [A]ROUND 
+//PSG.createPosition("WITHIN", "OUTSIDE"); // non-sense
+PSG.createPosition("WITHIN"); 
+PSG.createPosition("OUTSIDE"); 
 
 delete PSG.createPosition;
 
