8da8899b2364956b59f112654bf6eb635fcad0ac
Triston-Jerard: Taylor <pc.wiz.tt@gmail.com>
2019-01-04 10:26:23 -0600

    : SUPPORT
    FOR THE AUTOMATIC-CORRECT-CASE-MAPPING WITH THE FACT-EDITING

--- html/studio/script/psg-writer.js
+++ html/studio/script/psg-writer.js
@@ -300,5 +300,6 @@ function PhraseTableFactEditor() {
     editor.typeClaim = 7;
     editor.className = 'phrase-fact-editor';
     editor.setAttribute('spellcheck', 'true');
+    editor.oninput = function(){this.value = this.value.toUpperCase();};
     return editor;
 }
